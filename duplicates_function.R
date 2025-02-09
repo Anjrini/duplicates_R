35c33180beab20a423384b009476d1f08e4aeedf
Mustafa Anjrini <89596728+Anjrini@users.noreply.github.com>
2025-02-09 21:34:09 +0100

    Add files via upload

---
+++ duplicates_function.R
@@ -0,0 +1,42 @@
+# Mustafa Anjrini 09.02.2025
+# R script to get the duplicates and unique values
+
+dups<-function(df,n){ 
+  # provide the dataframe df as well as the number of the column
+  df<-as.data.frame(df)
+  df$countme<-0
+  df[is.na(df[,n]),n]<-"NA"
+  df[,n]<-sapply(df[,n], function(x) {tolower(x)})
+  df<-df[order(df[,n]),]
+  l<- dim(df)[2]
+  
+  for (i in 2:(length(df[,l])-1)) {
+    if(i==2 & df[i,n]==df[i-1,n] & df[i,n]==df[i+1,n]){
+      df[i-1,l]<-1
+      df[i,l]<-2
+      df[i+1,l]<-3
+    }
+    else if(i==2 & df[i,n]==df[i-1,n]){
+      df[i-1,l]<-1
+      df[i,l]<-2
+    }
+    else if (df[i,n]==df[i+1,n] & df[i,n]!=df[i-1,n]) {
+      df[i,l]<-1
+      df[i+1,l]<-df[i,l]+1
+    }
+    else if(df[i,n]==df[i+1,n] & df[i,n]==df[i-1,n]){
+      #df[i,l]<-df[i-1,l]+1
+      df[i+1,l]<-df[i,l]+1
+    }
+  }
+  dup<-df[df[,l]>0,]
+  no_dup<-df[df[,l]<2,]
+  z<-list(0,0)
+  z[[1]]<-no_dup[,-l]
+  z[[2]]<-dup[,-l]
+  return(z)
+  # The result is a list containing the z[[1]] the no duplicates value
+  # including one observation from each duplicate
+  # while the other list z[[2]] contains the duplicate values
+}
+
